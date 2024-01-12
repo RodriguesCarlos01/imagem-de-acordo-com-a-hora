4f17c26b3824130311728ebf74a44c2bfab870cc
RodriguesCarlos01 <cacaalcides7@gmail.com>
2024-01-12 17:02:57 -0300

    adicionando comentarios

--- script.js
+++ script.js
@@ -3,17 +3,17 @@ function carregar(){
     var img = document.querySelector("#imagem")
     var data = new Date()
     var hora = data.getHours()
-    // var hora = 17 , essa
+    // var hora = 17 , essa é uma variável de teste para forçar a mudança da imagem
     
     msg.innerHTML = `Agora são ${hora} horas`
-    if(hora >= 0 && hora < 12){
+    if(hora >= 0 && hora < 12){        // dentro deste intervalo de horas aparecerá uma igamen de bom dia
         img.src = "./src/manha.jpg"
         document.body.style.background = '#FFD700'
-    }else if(hora >= 12 && hora <= 18){
-        img.src = "./src/tarde.jpg"
+    }else if(hora >= 12 && hora <= 18){ 
+        img.src = "./src/tarde.jpg"  // dentro deste intervalo de horas aparecerá uma igamen de boa tarde
         document.body.style.background = '#A52A2A'
     }else{
-        img.src = "./src/noite.jpg"
+        img.src = "./src/noite.jpg"  // e aqui aparecerá uma ibagem de boa noite
         document.body.style.background = '#BA55d3'
     }
 }
